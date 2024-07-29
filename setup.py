101287b1e790577c054c7ef206a9ddbaf4b531c6
Joinck <167059959+Joinck@users.noreply.github.com>
2024-07-29 11:18:51 +0300

    setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'set_password=my_script:set_password',
+            'set_password=my_termux_repo.set_password:set_password',
         ],
     },
 )
